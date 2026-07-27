ba3e4290d010c08d0573cd25cd033e0ee7e82aaf
洛初 <githubcommit@luochu.cc>
2026-07-27 22:41:55 +0800

    add repository name env

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "./",
+  base: process.env.VITE_BASE || "./",
   plugins: [vue()],
   test: {
     environment: "jsdom",
